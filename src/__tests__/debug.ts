6b0b1d2909c5ec80581d4c06479e493c501f899d
Martynas Žilinskas <martynas@quatrodev.com>
2018-10-18 10:43:46 +0000

    Merged PR 380: Added additional method: deepUpdate.
    Related work items: #349

--- src/__tests__/debug.ts
+++ src/__tests__/debug.ts
@@ -3,11 +3,34 @@ import { NodeSettings } from "../settings/node-settings";
 
 async function main(): Promise<void> {
     setInterval(() => {}, 1000);
-
-    console.log("Started.");
-    const settings = await NodeSettings.init("settings.ini");
-    (global as any).settings = settings;
-    settings.on("updated", () => {});
+    // for (let i = 0; i < 2; i++) {
+    const settings2 = await NodeSettings.init(
+        "settings.ini",
+        {
+            blockchain: {
+                walletProviderUrl: "http://example.com:54321"
+            },
+            controller: {},
+            sockets: { http: {}, wrtc: {}, ws: {} },
+            ssl: {},
+            storage: {}
+        },
+        "settings2"
+    );
+    console.info(`node-id=${settings2.get("nodeId")}, wallet-provider-url=${settings2.getScope("blockchain").get("walletProviderUrl")}`);
+    // }
+    const settings = await NodeSettings.init(
+        "settings.ini",
+        {
+            blockchain: {},
+            controller: {},
+            sockets: { http: {}, wrtc: {}, ws: {} },
+            ssl: {},
+            storage: {}
+        },
+        "settings"
+    );
+    console.info(`node-id=${settings.get("nodeId")}, wallet-provider-url=${settings.getScope("blockchain").get("walletProviderUrl")}`);
 }
 
 main();
